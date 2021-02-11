99ac0ac245caa2bf3f09224def6cbe48d118cef6
Claudiu Nicola <claudiu@teamart.ro>
2021-02-11 11:59:45 +0200

    improvements

--- src/Components/project1.tsx
+++ src/Components/project1.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
 const Project1: React.FC = () => {
-    const [count, setCount] = useState<number>(0);
+    const [count, setCount] = useState(0);
+
+    const incrementCounter = () => setCount(prevState => prevState + 1);
+
     return (
-        <div style={{padding: '15px', textAlign:'center'}}>
+        <div style={{ padding: '15px', textAlign: 'center' }}>
             <input readOnly value={count} />
-            <button onClick={() => setCount(count + 1)} >Count</button>
+            <button onClick={incrementCounter} >Count</button>
         </div>
     );
 }
--- src/Components/project2.tsx
+++ src/Components/project2.tsx
@@ -1,46 +1,45 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 
 const Project2: React.FC = () => {
-    const [celsius, setCelsius] = useState<number>(0);
-    const [fahrenheit, setFahrenheit] = useState<number>(32);
-    const [colored, setColored] = useState<boolean>(false);
+    const [celsius, setCelsius] = useState(0);
+    const [fahrenheit, setFahrenheit] = useState(32);
+    const [errors, setErrors] = useState<boolean[]>([false, false]);
 
 
-    const styled = useMemo (() => ({
-        background: colored ? 'white' : 'red',
-        color: colored ? 'black' : 'white'
-    }), [colored]);
+    const styled = (index: number) => ({
+        background: errors[index] === false ? 'white' : 'red',
+        color: errors[index] === false ? 'black' : 'white'
+    });
 
     const toFahrenheit = (event: any) => {
         setCelsius(event.target.value);
-        if (Number(event.target.value)) {
-            setFahrenheit((event.target.value * 9/5) + 32);
-            setColored(true);
-        }
-        else if (String(event.target.value)){
-            setColored(false);
+        if (!isNaN(Number(event.target.value))) {
+            setFahrenheit((event.target.value * 9 / 5) + 32);
+            setErrors(prevState => ([false, false]));
+        } else {
+            setErrors(prevState => ([true, prevState[1]]));
         }
     };
+
     const toCelsius = (event: any) => {
         setFahrenheit(event.target.value);
-        if (Number(event.target.value)) {
-            setCelsius((event.target.value - 32) * 5/9);
-            setColored(true);
-        }
-        else if(String(event.target.value)) {
-            setColored(false);
+        if (!isNaN(Number(event.target.value))) {
+            setCelsius((event.target.value - 32) * 5 / 9);
+            setErrors(prevState => ([false, false]));
+        } else {
+            setErrors(prevState => ([prevState[0], true]));
         }
     };
-    
+
 
     return (
         <div style={{ padding: "15px", textAlign: "center" }}>
-            <input style={styled}
-                name="celsius" 
-                value={celsius} 
+            <input style={styled(0)}
+                name="celsius"
+                value={celsius}
                 onChange={toFahrenheit} />{" "}
             Celsius =
-            <input
+            <input style={styled(1)}
                 name="fahrenheit"
                 value={fahrenheit}
                 onChange={toCelsius}
